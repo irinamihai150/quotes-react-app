a6ec37c5755e83b76093e05742177013faa74e0c
irina mihai <irinamihai150@gmail.com>
2022-12-18 21:52:41 +0000

    delete unused var

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes} from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
